bd1b594f303a01d80c8659acfcc9746a439339a1
FelipeDP1015 <fe.delgadop@duocuc.cl>
2026-07-06 22:14:15 -0400

    Configurar perfil de pruebas con H2 en backends

--- backend/back-Despachos_SpringBoot/Springboot-API-REST-DESPACHO/src/test/java/com/citt/SpringbootApiRestDespachoApplicationTests.java
+++ backend/back-Despachos_SpringBoot/Springboot-API-REST-DESPACHO/src/test/java/com/citt/SpringbootApiRestDespachoApplicationTests.java
@@ -2,12 +2,14 @@ package com.citt;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SpringbootApiRestDespachoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
